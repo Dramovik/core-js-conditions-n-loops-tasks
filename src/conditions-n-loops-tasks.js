ed7237aec2a32ac992e4dcedb0dc8604d730593f
Dramovik <dramovik1@gmail.com>
2025-10-11 17:27:21 +0300

    feat: compleates isPositive, getMaxNumber, canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -21,8 +21,8 @@
  *  0  => true
  *  -5 => false
  */
-function isPositive(/* number */) {
-  throw new Error('Not implemented');
+function isPositive(number) {
+  return number >= 0;
 }
 
 /**
@@ -38,8 +38,14 @@ function isPositive(/* number */) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  if (a > b) {
+    return a;
+  }
+  if (b > c) {
+    return b;
+  }
+  return c;
 }
 
 /**
@@ -61,8 +67,27 @@ function getMaxNumber(/* a, b, c */) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x || queen.y === king.y) {
+    return true;
+  }
+  let xLeft = 1;
+  let yLeft = queen.y - (queen.x - xLeft);
+  for (xLeft; yLeft <= 8; xLeft += 1, yLeft += 1) {
+    if (king.x === xLeft && king.y === yLeft) {
+      return true;
+    }
+  }
+
+  let yRight = 1;
+  let xRight = queen.x + (queen.y - yRight);
+  for (yRight; xRight > 0; yRight += 1, xRight -= 1) {
+    if (king.x === xRight && king.y === yRight) {
+      return true;
+    }
+  }
+
+  return false;
 }
 
 /**
